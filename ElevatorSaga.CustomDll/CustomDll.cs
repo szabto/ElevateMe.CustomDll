f4c46c1ad045d82cf8704f8c1ea30f11d38acd64
Tamás <szabto@gmail.com>
2016-05-15 14:31:18 +0200

    Created a dummy custom dll

---
+++ ElevatorSaga.CustomDll/CustomDll.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ElevatorSaga.Core;
+using ElevatorSaga.Core.Classes;
+using ElevatorSaga.Core.Interfaces;
+
+namespace ElevatorSaga.CustomDll
+{
+    public class CustomDll : IWorld
+    {
+        private List<Floor> Floors;
+        private List<Elevator> Elevators;
+
+        public string Author { get { return "Tamás Szabó"; } }
+
+        public string AuthorEmail { get { return "gmail@szabto.com"; } }
+
+        public void Initialize()
+        {
+        }
+
+        public void WorldGenerated(List<Floor> f, List<Elevator> e)
+        {
+            Floors = f;
+            Elevators = e;
+        }
+
+        public void Update(int frame, World world)
+        {
+        }
+    }
+}
