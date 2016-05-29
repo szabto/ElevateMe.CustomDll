d3e735b626a7dabbfaa7abe2d0d1718563103a48
Tamás <szabto@gmail.com>
2016-05-29 19:32:34 +0200

    Small test case added
    Added when elevator is idle, go to random floor
    Added when floor button pressed, go to that floor (queued)
    Updated core library

--- ElevatorSaga.CustomDll/CustomWorld.cs
+++ ElevatorSaga.CustomDll/CustomWorld.cs
@@ -26,6 +26,28 @@ namespace ElevatorSaga.CustomDll
         {
             Floors = floors;
             Elevators = elevators;
+
+            foreach (Floor f in floors)
+            {
+                f.OnButtonPressed += OnFloorButtonPressed;
+            }
+
+            foreach (Elevator e in elevators)
+            {
+                e.Idle += OnElevatorIdle;
+            }
+        }
+
+        private static Random Rand = new Random();
+
+        private void OnElevatorIdle( object sender, EventArgs eargs)
+        {
+            Elevators[0].GoToFloor(Rand.Next(0, Floors.Count - 1));
+        }
+
+        private void OnFloorButtonPressed(object sender, FloorButtonPressedEventArgs eargs)
+        {
+            Elevators[0].GoToFloor(eargs.Floor.Level);
         }
 
         public void Update(int frame, World world)
